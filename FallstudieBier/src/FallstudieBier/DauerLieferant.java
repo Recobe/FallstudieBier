530ba0fadd55d74b485eb5e9466baf9fa73eb548
D060517 <D060517@141.72.115.143>
2014-10-20 22:15:37 +0200

    Kleine formale Änderungen

--- FallstudieBier/src/FallstudieBier/DauerLieferant.java
+++ FallstudieBier/src/FallstudieBier/DauerLieferant.java
@@ -9,11 +9,13 @@ public class DauerLieferant extends Lieferant {
 		
 	}
 	
-	public static float liefern(int menge) {
+	public float liefern(int menge) {
 		float kosten = menge * marktPreis;
 		return kosten;
 	}
 	
+	
+
 	public void pricing(int monat) {
 		if (monat == 8 || monat == 9) {
 			marktPreis = (float) 0.1;
@@ -22,5 +24,17 @@ public class DauerLieferant extends Lieferant {
 		}
 	}
 	
+	public static float getMarktPreis() {
+		return marktPreis;
+	}
+
+	public static void setMarktPreis(float marktPreis) {
+		DauerLieferant.marktPreis = marktPreis;
+	}
+
+	@Override
+	public String toString() {
+		return "DauerLieferant []";
+	}
 
 }
